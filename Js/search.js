e648c0a7c1e0e2a25866485fb2a4a8de8b3bc3ea
unknown <byadiso@gmail.com>
2023-10-24 11:27:58 +0200

    working-on-vocabulary-feature

--- Js/search.js
+++ Js/search.js
@@ -1,4 +1,32 @@
+// importing firebaseConfiguration and other configurations
 import { RapidAPI_Key } from "./apikey.js";
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+// import {
+//   getDatabase,
+//   set,
+//   ref,
+//   serverTimestamp,
+//   onValue,
+// } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+
+// import { FIREBASE_API_KEY } from "../Js/apikey.js";
+
+// const firebaseConfig = {
+//   apiKey: FIREBASE_API_KEY,
+//   authDomain: "mybrand-df7b7.firebaseapp.com",
+//   projectId: "mybrand-df7b7",
+//   storageBucket: "mybrand-df7b7.appspot.com",
+//   messagingSenderId: "1073877765217",
+//   appId: "1:1073877765217:web:7f63596f42c5d4ca18ae20",
+// };
+
+ // Initialize Firebase
+//  const app = firebase.initializeApp(firebaseConfig);
+//  const database = getDatabase(initializeApp(firebaseConfig));
+
+  
+
 document.addEventListener("DOMContentLoaded", () => {
   const searchResult = document.getElementById("search_content");
   const search = document.getElementById("search");
@@ -51,6 +79,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const response = await fetch(url, options);
         const result = await response.json();
         const data = result.results;
+        const Add_vocabulary_Btn = document.createElement("button");
+        Add_vocabulary_Btn.innerHTML="Save to vocabulary list";
+        Add_vocabulary_Btn.setAttribute("class", "Add_vocabulary_Btn");
         data.forEach((element) => {
           const definition = document.createElement("p");
           definition.innerHTML =
@@ -59,6 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
                          : </strong>` + element.definition.charAt(0).toUpperCase()+element.definition.slice(1) ;
           searchResult.append(definition);
         });
+        searchResult.append(Add_vocabulary_Btn);
       } catch (error) {
         errorDisplay.style.display = "block";
         errorDisplay.innerHTML = "Word not found try again";
@@ -92,4 +124,46 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   getSynonmy();
+
+
+  // adding vocabulary to the page
+  searchResult.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("Add_vocabulary_Btn")){
+      e.preventDefault()
+      addVocabulary()
+    }
+
+  }
+  )
+
+
+  // adding vocabulary
+
+  const addVocabulary = ()=>{ 
+    console.log("yes let add a vocabulary")
+    // firebase
+    // .database()
+    // .ref("vocabulary/")
+    // .push()
+    // .set(
+    //   {
+    //     title: title,
+    //     body: blogText,
+    //     createdAt: serverTimestamp(),
+    //     id: id,
+    //   },
+    //   function (error) {
+    //     if (error) {
+    //       console.log("error while adding vocabulary");
+    //     } else {
+    //       console.log("successfully added");
+
+    //     }
+    //   }
+    // )
+
+  }
+
+
+
 });
